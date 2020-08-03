cb4a9231f68b7c7bb2bbab6ddde344e40bbc44c0
imac <chadmdubay@gmail.com>
2020-08-03 16:44:56 -0400

    Updates

---
+++ precache-manifest.014ae28b64a561036268bc4c51a03185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d32bff7c8c04550fd6b9c88935e21d8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5d00a5559c983723efa",
+    "url": "/static/css/main.b7c8ae78.chunk.css"
+  },
+  {
+    "revision": "80165fa5a882675ec79f",
+    "url": "/static/js/2.11a63d68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11a63d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d00a5559c983723efa",
+    "url": "/static/js/main.60dc2403.chunk.js"
+  },
+  {
+    "revision": "c942f4d67e5599b14b27",
+    "url": "/static/js/runtime-main.e1106d7a.js"
+  }
+]);
